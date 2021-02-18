99b0ee60e1610052dbc1f339a52614f451b553aa
Adlo777 <loadgoca@gmail.com>
2021-02-18 13:20:23 +0100

    Second Commit

--- src/Exo1.java
+++ src/Exo1.java
@@ -1,4 +1,12 @@
 
 public class Exo1 {
-
+	public static void main (String[] args)
+	{
+		Rectangle rect1 = new Rectangle();
+		rect1.setHauteur(12);
+		rect1.setLargeur(21);
+		
+		System.out.println("La hauteur est " + rect1.getHauteur());
+		
+	}
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,31 @@
 
 public class Rectangle {
-
+	private double largeur;
+	private double hauteur;
+	
+	public double surface()
+	{
+		return hauteur*largeur;
+	}
+	
+	public void setHauteur(double h)
+	{
+		this.hauteur = h;
+		
+	}
+	
+	public void setLargeur(double l)
+	{
+		this.largeur = l;
+	}
+	
+	public double getHauteur()
+	{
+		return hauteur;
+	}
+	
+	public double getLargeur()
+	{
+		return largeur;
+	}
 }
